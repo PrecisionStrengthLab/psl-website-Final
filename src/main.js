c3aadeb8d5fefd37fe08b00e6ac02be6e05d5996
PrecisionStrengthLab <4zd8qzcx44@privaterelay.appleid.com>
2025-11-22 15:33:41 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './style.css'
+import App from './App'
+import { createElement } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('app')).render(
+  createElement(App)
+)
